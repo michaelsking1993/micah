f65f5154fdea2b338db6151ab91aa408f0278584
michael <michaelsking1993@gmail.com>
2019-03-18 15:55:06 -0400

    First commit with working AWS migration

--- db/migrate/20190317162912_add_references_to_tables.rb
+++ db/migrate/20190317162912_add_references_to_tables.rb
@@ -1,6 +1,5 @@
 class AddReferencesToTables < ActiveRecord::Migration[5.1]
   def change
-    add_reference :projects, :user, foreign_key: true
     add_reference :features, :project, foreign_key: true
     add_reference :steps, :feature, foreign_key: true
     add_reference :status_updates, :step, foreign_key: true
--- db/migrate/20190318180527_add_columns_to_user_for_authentication.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToUserForAuthentication < ActiveRecord::Migration[5.1]
-  def change
-    remove_reference :projects, :user, foreign_key: true
-    drop_table :users
-  end
-end
